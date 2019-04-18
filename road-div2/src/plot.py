3b985056dccb3b0ee66880aa0a8a8ad33cd7d57e
wx-csy <sy_chen@smail.nju.edu.cn>
2019-04-19 02:38:51 +0800

    misc changes

--- road-div2/src/plot.py
+++ road-div2/src/plot.py
@@ -16,9 +16,7 @@ for _ in range(n) :
     points.append([x, y])
     plt.scatter(x, y, c='b', s=5)
 
-tp, id1, id2 = map(int, input().split())
-plt.scatter(points[id1][0], points[id1][1], c='r', s=5)
-plt.scatter(points[id2][0], points[id2][1], c='r', s=5)
+input()
 
 p1x, p1y, p2x, p2y = map(float, input().split())
 plt.plot([p1x, p2x], [p1y, p2y])
