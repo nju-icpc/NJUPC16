d12ea2eb3c71796556c54fd98655d5852ecead2d
Shaoyuan Chen <sy_chen@smail.nju.edu.cn>
2019-04-19 11:30:52 +0800

    misc changes

--- road-div2/src/data/echelon.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-n = int(sys.argv[1])
-print(n + n)
-for i in range(n) :
-    print(i * 5000, -1000000 + i * 2000)
-    print(i * 5000, 1000000 - i * 2000)
--- road-div2/src/data/parallel.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-n = int(sys.argv[1])
-print(n + n)
-for i in range(n) :
-    print(i * 2000, -1000000)
-    print(i * 2000, 1000000)
